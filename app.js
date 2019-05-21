5c7dc0768a53e49dd2747d25cff6a5b167c49b39
aqdasmalik <aqdasmalikcs@gmail.com>
2019-05-21 15:48:05 +0500

    X-Real-IP

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ var app = express();
 app.use(requestIp.mw());
 const ips = ['127.0.0.1'];
 
-const customDetection = async req => {
+const customDetection = req => {
     let ipAddress = req.clientIp;
     console.log(`ipAddress: ${ipAddress}`);
+    console.log(`X-Real-IP: ${req.headers["X-Real-IP"]}`);
     return ipAddress;
 }
 
@@ -38,27 +39,27 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  let message = err.message;
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    let message = err.message;
     if (err instanceof IpDeniedError) {
-      message = 'You are not authorize to perform this action';
-      res.status(401);
-  } else {
-      res.status(err.status || 500)
-  }
+        message = 'You are not authorize to perform this action';
+        res.status(401);
+    } else {
+        res.status(err.status || 500)
+    }
 
-  // render the error page
-  res.render('error', {
-      message: message,
-      error: err
-  });
+    // render the error page
+    res.render('error', {
+        message: message,
+        error: err
+    });
 });
 
 module.exports = app;
