f039c7bcb9c53839de7a0194c1407565982c1617
aqdasmalik <aqdasmalikcs@gmail.com>
2019-05-21 15:46:00 +0500

    Proxy Wrap

--- bin/www.js
+++ bin/www.js
@@ -7,6 +7,10 @@
 var app = require('../app');
 var debug = require('debug')('ipfiltering:server');
 var http = require('http');
+var proxiedHttp = require('findhit-proxywrap').proxy(http, {
+  strict: true,
+  overrideRemote: false
+})
 
 /**
  * Get port from environment and store in Express.
@@ -19,7 +23,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app);
+var server = proxiedHttp.createServer(app)
 
 /**
  * Listen on provided port, on all network interfaces.
